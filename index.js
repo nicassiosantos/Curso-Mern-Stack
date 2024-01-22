fc0edc9f16e49a526e2a5e747a7028ef1b04e5bd
Antonio Nicassio <antonio.nicassio007@hotmail.com>
2024-01-22 11:24:33 -0300

    Comentários de identificação do que é uma rota

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const app = express()
 
-app.get('/home', (req, res) => {
-  res.send('Hello World')
-})
+//ROTA 
+  // Method HTTP - CRUD(CREATE, READ, UPDATE, DELETE) 
+    // GET - pega uma info 
+    // POST - Cria uma info 
+    // PUT - Altera toda a info 
+    // PATH - Altera parte da info 
+    // DELETE - Apaga uma info
+
+  // Name - Um identificador da rota  
+
+  // Function(Callback) - Responsavel por executar algum comando 
+
+app.get('/soma', (req, res) => {
+  const soma = 1 + 1; 
+  res.json({soma: soma})
+
+});
 
 app.listen(3000)

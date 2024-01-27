28ce527e12b01e1a243bf86aa905107005b80230
Antonio Nicassio <antonio.nicassio007@hotmail.com>
2024-01-27 10:01:37 -0300

    Conectando com MongoDB Atlas
    Conexão com o banco de dados e Criando Model e serviços para criação de um User

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express(); 
+const connectDatbase = require("./src/database/db");
 
-const userRoute = require("./src/routes/user.route")
+const userRoute = require("./src/routes/user.route");
 
 const port = 3000;
 
+connectDatbase();
 app.use(express.json());
 app.use("/user", userRoute);
 
--- src/controlers/user.controller.js
+++ src/controlers/user.controller.js
@@ -1,25 +1,31 @@
+const userService = require("../services/user.services");
 
-const create = (req, res) =>{ 
-    const {name, username, email, password, avatar, background}= req.body; 
+const create = async(req, res) => {
+    const { name, username, email, password, avatar, background } = req.body;
 
-    if(!name ||!username ||!email ||!password ||!avatar ||!background){
-        res.status(400).send({message: "Submit all fields for registration"});
+    if (!name || !username || !email || !password || !avatar || !background) {
+        res.status(400).send({ message: "Submit all fields for registration" });
+    }
+
+    const user = await userService.create(req.body);
+
+    if(!user){
+        return res.status(400).send( { message: "Error creating User" });
     }
 
     res.status(201).send({
-
         message: "User created sucessfully",
-
-        user:{
-            name, 
+        user: {
+            id: user._id,
+            name,
             username,
-            email,  
-            avatar, 
+            email,
+            avatar,
             background
-        } 
+        },
 
-    
-    }); 
+
+    });
 
 };
 

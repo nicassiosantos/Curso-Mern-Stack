fe8f071bdb301d5574e94997044a992af8329b9d
Antonio Nicassio <antonio.nicassio007@hotmail.com>
2024-01-27 10:19:17 -0300

    alterações

--- src/database/db.js
+++ src/database/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDatbase = () => {
     console.log("Wait connecting to the database");
 
-    mongoose.connect("mongodb+srv://antnicassio:STJKuAM10kBRTbRL@cluster0.ifzw2l4.mongodb.net/?retryWrites=true&w=majority",
+    mongoose.connect("mongodb+srv://antnicassio:<password>@cluster0.ifzw2l4.mongodb.net/?retryWrites=true&w=majority",
         { useNewUrlParser: true, useUnifiedTopology: true }
     ).then(() => console.log("MongoDB Atlas connected")).catch((error) => console.log(error));
 };

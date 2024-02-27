78fc752a325974c62d940135c46ce292c3498e31
Antonio Nicassio <antonio.nicassio007@hotmail.com>
2024-02-27 12:20:52 -0300

    Atualizando informação do Usuário pelo Id

--- src/controlers/user.controller.js
+++ src/controlers/user.controller.js
@@ -1,7 +1,7 @@
 const userService = require("../services/user.services");
 const mongoose = require("mongoose");
 
-const create = async(req, res) => {
+const create = async (req, res) => {
     const { name, username, email, password, avatar, background } = req.body;
 
     if (!name || !username || !email || !password || !avatar || !background) {
@@ -10,8 +10,8 @@ const create = async(req, res) => {
 
     const user = await userService.createService(req.body);
 
-    if(!user){
-        return res.status(400).send( { message: "Error creating User" });
+    if (!user) {
+        return res.status(400).send({ message: "Error creating User" });
     }
 
     res.status(201).send({
@@ -28,34 +28,68 @@ const create = async(req, res) => {
 
     });
 
-}; 
+};
 
-const findAll = async(req, res) =>{
+const findAll = async (req, res) => {
     const users = await userService.findAllService();
 
-    if(users.lenght === 0){
+    if (users.lenght === 0) {
         return res.status(400).send({ message: "There are no registered users" })
     }
 
     res.send(users);
-} 
+};
 
-const findById = async(req, res) =>{
+const findById = async (req, res) => {
     const id = req.params.id;
 
-    if(!mongoose.Types.ObjectId.isValid(id)){
+    if (!mongoose.Types.ObjectId.isValid(id)) {
         return res.status(400).send({ message: "invalid Id" })
     }
 
     const user = await userService.findByIdService(id);
 
-    if(!user){
+    if (!user) {
         return res.status(400).send({ message: "User not found" })
     }
 
     res.send(user);
-}
+};
+
+const update = async (req, res) => {
+    const { name, username, email, password, avatar, background } = req.body;
+
+    if (!name && !username && !email && !password && !avatar && !background) {
+        res.status(400).send({ message: "Submit at least one field for update" });
+    }
+
+    const id = req.params.id;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).send({ message: "invalid Id" })
+    }
+
+    const user = await userService.findByIdService(id);
+
+    if (!user) {
+        return res.status(400).send({ message: "User not found" })
+    }
+
+    await userService.updateService(
+        id,
+        name, 
+        username, 
+        email, 
+        password, 
+        avatar, 
+        background
+    ); 
+
+    res.send({message: "User sucessfully updated"})
+
+
+};
 
 
 
-module.exports = { create, findAll, findById }; 
+module.exports = { create, findAll, findById, update }; 
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -4,5 +4,6 @@ const userController = require('../controlers/user.controller')
 route.post("/", userController.create);
 route.get("/", userController.findAll);
 route.get("/:id", userController.findById);
+route.patch("/:id", userController.update);
 
 module.exports = route;

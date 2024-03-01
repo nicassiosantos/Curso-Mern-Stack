65008d5776d8c09e8132147197c9d24156036cca
Antonio Nicassio <antonio.nicassio007@hotmail.com>
2024-03-01 17:08:32 -0300

    Adicionando variaveis globais através do .env

--- src/database/db.js
+++ src/database/db.js
@@ -3,8 +3,11 @@ import mongoose from 'mongoose';
 const connectDatbase = () => {
     console.log("Wait connecting to the database");
 
-    mongoose.connect("mongodb+srv://antnicassio:<password>@cluster0.ifzw2l4.mongodb.net/?retryWrites=true&w=majority",
-        { useNewUrlParser: true, useUnifiedTopology: true }
+    mongoose.connect(process.env.MONGODB_URI,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }
     ).then(() => console.log("MongoDB Atlas connected")).catch((error) => console.log(error));
 };
 

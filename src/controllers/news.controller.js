108a0d82de4295a3149e76d954345c85499b0a14
Antonio Nicassio <antonio.nicassio007@hotmail.com>
2024-03-04 17:38:25 -0300

    adicionando paginação de dados
    ao pegar as items ou dados em um projeto, é importante criar a paginação de dados, para que em uma aplicação que contém muitos dados não sobrecarregue o cliente, assim repartindo os dados que chegam

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -1,4 +1,4 @@
-import { createService, findAllService } from "../services/news.service.js"
+import { createService, findAllService, countNews } from "../services/news.service.js"
 import { ObjectId } from "mongoose";
 
 const create = async (req, res) => {
@@ -25,13 +25,53 @@ const create = async (req, res) => {
 
 const findAll = async (req, res) => {
     try {
-        const news = await findAllService();
+        let {limit, offset} = req.query; 
+        
+        limit = Number(limit);
+        offset = Number(offset);
+        
+
+        if(!limit){ 
+            limit = 5;
+        }
+
+        if(!offset){ 
+           offset = 0;
+        }
+
+        const news = await findAllService(offset, limit);
+        const total = await countNews(); 
+        const currentUrl = req.baseUrl;
+
+        const next = offset + limit; 
+        const nextUrl = next < total? `${currentUrl}?limit=${limit}&offset=${offset}` : null;
+
+        const previous = offset - limit < 0 ? null : offset - limit; 
+        const previousUrl = previous!=null ? `${currentUrl}?limit=${limit}&offset=${previous}` : null;
 
         if (news.lenght === 0) {
             return res.status(400).send({ message: "There are no registered news" })
         } 
 
-        res.send(news); 
+        res.send({
+            nextUrl, 
+            previousUrl, 
+            limit, 
+            offset,
+            total, 
+
+            results: news.map(item => ({
+                id: item._id, 
+                title: item.title, 
+                text: item.text, 
+                banner: item.banner, 
+                likes: item.likes, 
+                comments: item.comments, 
+                name: item.user.name,
+                username: item.user.username, 
+                userAvatar: item.user.avatar,
+            }))
+        }); 
 
 
     } catch (err) {

e9644213813d7acd746750fa80dd34f552e03263
Antonio Nicassio <antonio.nicassio007@hotmail.com>
2024-03-05 20:47:01 -0300

    busca da news pelo titulo
    utilizando o QueryString

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -1,4 +1,4 @@
-import { createService, findAllService, countNews, topNewsService, findByIdService } from "../services/news.service.js"
+import { createService, findAllService, countNews, topNewsService, findByIdService, searchByTitleService } from "../services/news.service.js"
 import { ObjectId } from "mongoose";
 
 const create = async (req, res) => {
@@ -130,6 +130,35 @@ const findById = async (req, res) =>{
         res.status(500).send({ message: err.message })
     }
 
-};
+}; 
 
-export { create, findAll, topNews, findById }
+const searchByTitle = async (req, res)=>{
+    try{ 
+        const {title} = req.query; 
+
+        const news = await searchByTitleService(title);
+
+        if(news.length === 0){ 
+            return res.status(400).send({message: "There are no news with this title"})
+        }
+
+        return res.send({
+            results: news.map(item => ({
+                id: item._id,
+                title: item.title,
+                text: item.text,
+                banner: item.banner,
+                likes: item.likes,
+                comments: item.comments,
+                name: item.user.name,
+                username: item.user.username,
+                userAvatar: item.user.avatar,
+            }))
+        });
+
+    } catch(err){
+        res.status(500).send({ message: err.message })
+    }
+}
+
+export { create, findAll, topNews, findById, searchByTitle }

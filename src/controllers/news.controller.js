426b0e96a0a388e22bf3f099b17f7b1a9cf7bbf2
Antonio Nicassio <antonio.nicassio007@hotmail.com>
2024-03-05 21:54:08 -0300

    busca news pelo id do usuário

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -1,4 +1,4 @@
-import { createService, findAllService, countNews, topNewsService, findByIdService, searchByTitleService } from "../services/news.service.js"
+import { createService, findAllService, countNews, topNewsService, findByIdService, searchByTitleService, byUserService } from "../services/news.service.js"
 import { ObjectId } from "mongoose";
 
 const create = async (req, res) => {
@@ -106,9 +106,9 @@ const topNews = async (req, res) => {
     }
 };
 
-const findById = async (req, res) =>{ 
+const findById = async (req, res) => {
     try {
-        const { id } = req.params; 
+        const { id } = req.params;
 
         const news = await findByIdService(id);
 
@@ -126,20 +126,20 @@ const findById = async (req, res) =>{
             },
         });
 
-    } catch (err){ 
+    } catch (err) {
         res.status(500).send({ message: err.message })
     }
 
-}; 
+};
 
-const searchByTitle = async (req, res)=>{
-    try{ 
-        const {title} = req.query; 
+const searchByTitle = async (req, res) => {
+    try {
+        const { title } = req.query;
 
         const news = await searchByTitleService(title);
 
-        if(news.length === 0){ 
-            return res.status(400).send({message: "There are no news with this title"})
+        if (news.length === 0) {
+            return res.status(400).send({ message: "There are no news with this title" })
         }
 
         return res.send({
@@ -156,9 +156,34 @@ const searchByTitle = async (req, res)=>{
             }))
         });
 
-    } catch(err){
+    } catch (err) {
         res.status(500).send({ message: err.message })
     }
 }
 
-export { create, findAll, topNews, findById, searchByTitle }
+const byUser = async (req, res) => {
+    try {
+        const id = req.userId; 
+        const news = await byUserService(id);
+
+        return res.send({
+            results: news.map(item => ({
+                id: item._id,
+                title: item.title,
+                text: item.text,
+                banner: item.banner,
+                likes: item.likes,
+                comments: item.comments,
+                name: item.user.name,
+                username: item.user.username,
+                userAvatar: item.user.avatar,
+            }))
+        });
+
+    } catch (err) {
+        res.status(500).send({ message: err.message })
+    }
+}
+
+
+export { create, findAll, topNews, findById, searchByTitle, byUser }
